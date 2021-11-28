63c148473d2d066297cc54b9784cfc7c5e883a97
Yurino Oga <yurino.o@latona.io>
2021-11-28 13:31:53 +0900

    release

--- sap-purchasing-source-list.sql
+++ sap-purchasing-source-list.sql
@@ -2,7 +2,7 @@ CREATE TABLE `sap-purchasing-source-list`
 (
     `Material`              varchar(40) DEFAULT NULL,
     `Plant`                 varchar(4) DEFAULT NULL,
-    `SourceListRecord`      int(5) DEFAULT NULL,
+    `SourceListRecord`      varchar(5) DEFAULT NULL,
     `ValidityStartDate`     date DEFAULT NULL,
     `ValidityEndDate`       date DEFAULT NULL,
     `Supplier`              varchar(10) DEFAULT NULL,
@@ -10,13 +10,13 @@ CREATE TABLE `sap-purchasing-source-list`
     `SupplyingPlant`        varchar(4) DEFAULT NULL,
     `OrderQuantityUnit`     varchar(3) DEFAULT NULL,
     `PurchaseOutlineAgreement` varchar(10) DEFAULT NULL,
-    `SupplierIsFixed`       varchar(1) DEFAULT NULL,
-    `SourceOfSupplyIsBlocked` varchar(1) DEFAULT NULL,
+    `SupplierIsFixed`       tinyint(1) DEFAULT NULL,
+    `SourceOfSupplyIsBlocked` tinyint(1) DEFAULT NULL,
     `MRPSourcingControl`    varchar(1) DEFAULT NULL,
     `LastChangeDateTime`    datetime DEFAULT NULL,
-    `IssgPlantIsFixed`      varchar(1) DEFAULT NULL,
-    `PurOutlineAgreementIsFixed` varchar(1) DEFAULT NULL,
-    `SourceOfSupplyIsFixed` varchar(1) DEFAULT NULL,
+    `IssgPlantIsFixed`      tinyint(1) DEFAULT NULL,
+    `PurOutlineAgreementIsFixed` tinyint(1) DEFAULT NULL,
+    `SourceOfSupplyIsFixed` tinyint(1) DEFAULT NULL,
     PRIMARY KEY (`Material`, `Plant`,`SourceListRecord`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
